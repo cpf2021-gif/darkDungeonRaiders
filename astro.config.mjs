a12e79a914d0de1e572778fdae1dca4057911a25
cpf2021-gif <2992247892@qq.com>
2023-09-07 12:15:23 +0800

    feat: init doc

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Doc',
+			social: {
+				github: 'https://github.com/cpf2021-gif',
+			},
+			locales: {
+				root: {
+					label: '简体中文',
+					lang: 'zh-CN',
+				}
+			},
+			sidebar: [
+				{
+					label: '介绍',
+					autogenerate: { directory: 'about' },
+				},
+			],
+			lastUpdated: true,
+			pagination: true,
+		}),
+	],
+
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
